5a59a327845829f1a14e29821ea4880953e9463c
daviotavio <daviotavioribeiro@gmail.com>
2022-12-18 21:21:33 -0300

    Atividade 3

---
+++ Exercises/003.py
@@ -0,0 +1,8 @@
+"""Exercicio 003
+Faça um Programa que peça dois números e imprima a soma."""
+
+n1 = int(input('Digite o primero número: '))
+n2 = int(input('Digite o segundo número: '))
+soma = n1 + n2
+
+print(f'A soma dos valores é: {soma}')
