55741a998162e6de8e9636475cf055037289d456
daviotavio <daviotavioribeiro@gmail.com>
2022-12-18 21:22:02 -0300

    Atividade 7

---
+++ Exercises/007.py
@@ -0,0 +1,7 @@
+"""Exercicio 007
+Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário."""
+
+lado = float(input('Entre com o valor do lado do quarado: '))
+area = lado ** 2
+
+print(f'A área do quadrado é: {area}, e o seu dobro é {area * 2}: ')
