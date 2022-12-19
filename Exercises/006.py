78416e35e65d84478fe70ec7c5f7ce84d1d6194d
daviotavio <daviotavioribeiro@gmail.com>
2022-12-18 21:21:53 -0300

    Atividade 6

---
+++ Exercises/006.py
@@ -0,0 +1,7 @@
+"""Exercicio 006
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área."""
+
+raio = float(input('Forneça o raio do círculo'))
+area = 3.1415 * (raio ** 2)
+
+print(f'A área do circulo é:{area}')
