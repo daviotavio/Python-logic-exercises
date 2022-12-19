432b9c731dc043b57eaeb8f08b76728b589a2a1b
daviotavio <daviotavioribeiro@gmail.com>
2022-12-18 21:21:43 -0300

    Atividade 5

---
+++ Exercises/005.py
@@ -0,0 +1,7 @@
+"""Exercicio 005
+Faça um Programa que converta metros para centímetros."""
+
+medida = float(input('Entre com o valor em metros: '))
+cm = medida * 100
+
+print(f'A medida {medida}m em centímetros é igual à {cm}cm')
